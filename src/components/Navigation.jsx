157746e6e8478f8512226b250d0c97259da78f88
fatah0003 <fatahutd@gmail.com>
2025-09-03 14:26:07 +0200

    PostPages and PostdetailsPage created with api

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,9 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => (
-    <nav>
-        <NavLink to="/">Posts</NavLink> |{' '}
-    </nav>
+  <nav>
+    <NavLink to="/">Posts</NavLink>
+  </nav>
 );
 
 export default Navigation;
