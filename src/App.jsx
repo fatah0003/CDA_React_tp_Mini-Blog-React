e38029c7fffc4e1d086476c6dd5878f193cbf4e3
fatah0003 <fatahutd@gmail.com>
2025-09-03 17:01:01 +0200

    l'ajout des posts grace au localstorage, et syle basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Navigation from './components/Navigation';
 import PostsPage from './pages/PostsPage';
 import PostDetailPage from './pages/PostDetailPage';
@@ -7,14 +7,14 @@ import './App.css';
 
 function App() {
     return (
-        <Router>
+        <>
             <Navigation />
             <Routes>
                 <Route path="/" element={<PostsPage />} />
                 <Route path="/post/:id" element={<PostDetailPage />} />
                 <Route path="/create" element={<CreatePostPage />} />
             </Routes>
-        </Router>
+        </>
     );
 }
 
